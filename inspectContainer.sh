1bf7e1cef5275e813907567b0d3a16af0f740e0b
Adrian Biro <biroadrian@protonmail.com>
2023-04-25 20:27:05 +0200

    small fixes

--- inspectContainer.sh
+++ inspectContainer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+#inspect
 curl -s "http://0.0.0.0:2376/containers/$(curl -s http://0.0.0.0:2376/containers/json | jq -r '.[0].Id')/json"
 # stats
 curl -s "http://0.0.0.0:2376/containers/$(curl -s http://0.0.0.0:2376/containers/json | jq -r '.[0].Id')/stats" 
+# kill
+curl -s -XPOST "http://0.0.0.0:2376/containers/$(curl -s http://0.0.0.0:2376/containers/json | jq -r '.[0].Id')/kill"
